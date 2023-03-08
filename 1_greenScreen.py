f01cbe133b1abd30e689e9403ad0d5d63c2b7410
Yuri Fujii <49227941+Reetos@users.noreply.github.com>
2023-03-08 21:53:17 +0900

    the first implementation

--- 1_greenScreen.py
+++ 1_greenScreen.py
@@ -1,4 +1,32 @@
 import cv2
 import numpy as np
 
+video = cv2.VideoCapture("sofa-11294.mp4")
+image = cv2.imread("sunset-g9d1b19c70_1920.jpg")
 
+while True:
+
+    ret, frame = video.read()
+    #print(f'frame size: {frame.shape}')
+
+    image = cv2.resize(image, (1920, 1080))
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    u_green = np.array([104, 153, 70])
+    l_green = np.array([30,30,0])
+
+    mask = cv2.inRange(frame, l_green, u_green)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    f = frame - res
+    f = np.where(f==0, image, f)
+
+    cv2.imshow("video", frame)
+    cv2.imshow("mask", f)
+
+    if cv2.waitKey(25) == 27:
+        break
+
+video.release()
+cv2.destroyAllWindows()
---
+++ pixel_value.py
@@ -0,0 +1,27 @@
+from ipywidgets import widgets, fixed
+
+
+def inRange(img, c1, c2, c3):
+    """2値化処理を行い、結果を表示する。
+    """
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array([c1[0], c2[0], c3[0]])
+    upper = np.array([c1[1], c2[1], c3[1]])
+
+    bin_img = cv2.inRange(hsv, lower, upper)
+    imshow(bin_img)
+# パラメータ lower, upper を設定するスライダー
+names = ["H", "S", "V"]
+parts = {}
+for i, name in enumerate(names, 1):
+    slider = widgets.SelectionRangeSlider(
+        options=np.arange(256), index=(0, 255), description=name
+    )
+    slider.layout.width = "400px"
+    parts[f"c{i}"] = slider
+
+# 画像を読み込む。
+img = cv2.imread("sample.jpg")
+
+# ウィジェットを表示する。
+widgets.interactive(inRange, **parts, img=fixed(img))
