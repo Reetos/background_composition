c85dd1dd2751bd07c89c3e4793fbb1acbd36c2c3
Yuri Fujii <49227941+Reetos@users.noreply.github.com>
2023-03-09 03:17:02 +0900

    chatgpt

---
+++ 2_gsRemoval.py
@@ -0,0 +1,22 @@
+import cv2
+
+video = cv2.VideoCapture("sofa-11294.mp4")
+image = cv2.imread("sunset-g9d1b19c70_1920.jpg")
+
+cv2.namedWindow("Trackbars")
+cv2.resizedWindow("rackbars", 300, 300)
+
+cv2.createTrACKBAR("L - V", "Trackbars", 0,aa)
+while True:
+    ret,frame = video.read()
+    frame = cv2.resize(frame, (640,480))
+    image = cv2.resize(image, (640,480))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    mask = cv2.inRange(hsv, )
+    cv2.imshow("Frame",frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
